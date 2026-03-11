23cb88a911592f4c3602f57ffc9ee53f5c13da19
ValenPaul <valenpaul02@gmail.com>
2026-03-11 08:05:27 -0300

    new archivo webhook

--- src/backend/routes/compras.js
+++ src/backend/routes/compras.js
@@ -78,7 +78,7 @@ router.post('/crear-preferencia', verificarToken, async (req, res) => {
 
                 auto_return: 'approved',
 
-                notification_url: `https://rutinas-de-gimnasio-git-prueba-valenpauls-projects.vercel.app/api/webhook`,
+                notification_url: `https://primeconditioning.vercel.app/api/webhook`,
             }
         });
 
